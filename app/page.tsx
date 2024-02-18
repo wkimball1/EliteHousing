f0f18829fa295f8349b4f9032b435e0485122a62
William Kimball <willkimball99@gmail.com>
2024-02-17 22:12:39 -0500

    removed component/header

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 import { createClient } from "@/utils/supabase/server";
-import Header from "@/components/Header";
 
 export default async function Index() {
   const canInitSupabaseClient = () => {
