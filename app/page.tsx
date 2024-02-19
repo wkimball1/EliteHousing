afec73cab8b413d1ecefcf2e07b28937623b8747
William Kimball <willkimball99@gmail.com>
2024-02-19 01:01:57 -0500

    removed ts check

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,6 @@ export default async function Index() {
   return (
     <div className="flex flex-col w-full bg-background">
       <NavBar>
-        {/* @ts-expect-error Server Component */}
         <AuthButton/ >
       </NavBar>
       <div className="flex-1 w-full flex flex-col gap-20 items-center">
